2ac9c2ca7beb533f15a5c00655bd483f2bf05cdf
liabao <zhangyang06@bilibili.com>
2020-12-10 12:48:08 +0800

    [feat]  子线程启动activity 崩溃问题修复

--- CoroutineSupport/src/main/java/com/kronos/router/coroutine/CoroutineSupportExtension.kt
+++ CoroutineSupport/src/main/java/com/kronos/router/coroutine/CoroutineSupportExtension.kt
@@ -2,13 +2,22 @@ package com.kronos.router.coroutine
 
 import android.content.Context
 import com.kronos.router.KRequest
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import kotlin.coroutines.resume
 
 /**
  * @Author LiABao
  * @Since 2020/12/10
  */
+suspend fun KRequest.dispatcher(context: Context): Boolean {
+    return withContext(Dispatchers.Main) {
+        val result = await(context)
+        result
+    }
+}
+
 suspend fun KRequest.await(context: Context): Boolean {
     return suspendCancellableCoroutine { continuation ->
         onSuccess = {
