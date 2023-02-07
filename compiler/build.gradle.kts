2de8346b1a80739d12bbde1ee7525f49aab36160
zhangyang06 <zhangyang06@bilibili.com>
2023-02-07 17:40:08 +0800

    [feat]  软连接改造 compiler

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -6,12 +6,17 @@ plugins {
     id("kotlin-kapt")
 }
 
+java.sourceSets["main"].java {
+    srcDir("src/link/main")
+}
+
+
 dependencies {
     implementation(libs.auto.service)
     implementation(libs.javapoet)
     implementation(libs.commons.lang3)
     implementation(libs.commons.collections4)
-    implementation(libs.annotation)
+        // implementation(libs.annotation)
     implementation(files(org.gradle.internal.jvm.Jvm.current().toolsJar))
     implementation(libs.kotlin.stdlib.jdk7)
     kapt(libs.auto.service)
